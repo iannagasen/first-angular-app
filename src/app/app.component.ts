e3a89fb02e4cead19f004b3d9f557d106f08901c
iann.agasen <iann.agasen@gmail.com>
2023-08-27 10:26:10 +0800

    input binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+// By Convention this AppComponent is the main module
+// Main Module -> starting point of an app
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-angular-app';
+  title = 'first-angular-app-changed';
+  description = 'Hello World';
 }
